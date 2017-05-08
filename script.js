ce1ece46da792f01c50a8b5c78a74f1fb09d7b3a
Jerome Foster II <jfoster@wlapcs.org>
2017-05-08 15:51:24 +0000

    cahnges the shape from squares to circles

--- script.js
+++ script.js
@@ -4,19 +4,23 @@ for (var i = 0; i < 1890; i++) {
     var myrotate = document.createElement('a-animation');
     myrotate.setAttribute('attribute',"rotation");
     myrotate.setAttribute('dur',"5000");
-    myrotate.setAttribute('to',"0 360 0");
+    myrotate.setAttribute('to',"0 180 0");
     
     mybox.appendChild(myrotate);
     
-    mybox.setAttribute('geometry',"primitive:box");
+    mybox.setAttribute('geometry',"primitive:sphere");
+    
     
     var x = (Math.random()*500).toString();
     var y = (Math.random()*500).toString();
     var z = (Math.random()*500).toString();
     
-    mybox.setAttribute('position',x+" "+y+" "+z)
+    mybox.setAttribute('position',x+" "+y+" "+z);
     
     var myscene = document.querySelector('a-scene');
     
     myscene.appendChild(mybox);
+}
+for(var p = 0; p < 50; p++){
+    
 }
